f390be280d6e306fed69a96b7d8a2c8e34b6ab8e
Andrea Baccega <vekexasia@gmail.com>
2024-06-15 17:37:55 +0200

    aa

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -6,3 +6,4 @@
 &kp EXCL,          &kp UNDER,         &kp COLON,         &kp EQUAL,         &kp DLLR,          &kp AT,            &kp LPAR,          &kp RPAR,          &kp UNDER,         &kp SEMI,          \
 &kp PRCNT,         &kp QMARK,         &kp STAR,          &kp PLUS,          &kp BSLH,          &kp FSLH,          &kp MINUS,         &kp LT,            &kp GT,            &kp DQT,              \
 U_NP,              U_NP,              U_NP,              U_NP,              U_NP,              U_NP,              U_NP,              U_NP,              U_NP,              U_NP
+sxdfsdfsdf
