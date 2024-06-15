437b0553dbd75ea41ef673a55543d51cbffabb2d
Andrea Baccega <vekexasia@gmail.com>
2024-06-15 22:37:37 +0200

    added test maceo

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,7 +1,14 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
+
 #define MIRYOKU_LAYER_SYM \
 &kp GRAVE,         &kp TILDE,         &kp HASH,          &kp AMPS,          &kp PIPE,          &kp CARET,         &kp LBRC,          &kp RBRC,          &kp LBKT,          &kp RBKT,            \
 &kp EXCL,          &kp UNDER,         &kp COLON,         &kp EQUAL,         &kp DLLR,          &kp AT,            &kp LPAR,          &kp RPAR,          &kp UNDER,         &kp SEMI,          \
 &kp PRCNT,         &kp QMARK,         &kp STAR,          &kp PLUS,          &kp BSLH,          &kp FSLH,          &kp MINUS,         &kp LT,            &kp GT,            &kp DQT,              \
 U_NP,              U_NP,              U_NP,              U_NP,              U_NP,              U_NP,              U_NP,              U_NP,              U_NP,              U_NP
+
+ZMK_MACRO(arrowfn,
+        wait-ms = <30>;
+        tap-ms = <40>;
+        bindings = <&kp EQUAL &kp GT &kp SPACE &kp LBRC &kp RBRC &kp LEFT &kp RET>;
+    )
