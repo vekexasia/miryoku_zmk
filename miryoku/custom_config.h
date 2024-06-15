94133c8a7df73eaae5750e9a983e0bcc4002ed7e
Andrea Baccega <vekexasia@gmail.com>
2024-06-15 17:51:36 +0200

    fix

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,7 +1,7 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 #define MIRYOKU_LAYER_SYM \
-&kp GRAVE,         &kp TILDE,         &kp NON_US_HASH,   &kp AMPS,          &kp PIPE2,         &kp CARET,         &kp LBRC,          &kp RBRC,          &kp LBKT,          &kp RBKT,            \
+&kp GRAVE,         &kp TILDE,         &kp HASH,          &kp AMPS,          &kp PIPE,          &kp CARET,         &kp LBRC,          &kp RBRC,          &kp LBKT,          &kp RBKT,            \
 &kp EXCL,          &kp UNDER,         &kp COLON,         &kp EQUAL,         &kp DLLR,          &kp AT,            &kp LPAR,          &kp RPAR,          &kp UNDER,         &kp SEMI,          \
 &kp PRCNT,         &kp QMARK,         &kp STAR,          &kp PLUS,          &kp BSLH,          &kp FSLH,          &kp MINUS,         &kp LT,            &kp GT,            &kp DQT,              \
 U_NP,              U_NP,              U_NP,              U_NP,              U_NP,              U_NP,              U_NP,              U_NP,              U_NP,              U_NP
